6dd034cfb1de993b9ca984d654ff5331c1c6fa3a
Jessy Ayala <jayala_29@hotmail.com>
2023-02-13 10:20:39 -0800

    added comments to array file

--- array.py
+++ array.py
@@ -1,7 +1,10 @@
 import random
 arr = []
+# generate a random array of length k
 for k in range(int(random.randint(1,10))):
+	# give the array random values
 	arr.append(int(random.randint(0,100)))
 print("Here's an array: " + str(arr))
+# ask for index value
 i = input("Which index would you like printed out? ")
 print("Okay, the value at the index you requested is: " + str(arr[int(i)]))
